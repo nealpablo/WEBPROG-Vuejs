81a77e4040acf8760fe8ca982f5ccecdd63419c5
nealpablo <idpablo@student.apc.edu.ph>
2025-02-10 05:05:34 +0800

    added personal website

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="container">
+        <header>
+            <h1>Personal Profile Web Page</h1>
+        </header>
+        <main>
+            <section class="main-content">
+                <div>
+                    <h2>ABOUT ME</h2>
+                    <img src="https://media.discordapp.net/attachments/1303410468060266567/1319556517086625833/IMG_2975.jpg?ex=676cfbc5&is=676baa45&hm=12a01aa75d2f88c965b8031a7c0baa3bb671c0064a1fbf7c33b103ec848f00bf&=&format=webp&width=672&height=897">
+                    <p>Name: Ishmael Neal Pablo</p>
+                    <p>Nickname: Neal/Pablo</p>
+                </div>
+                <div>
+                    <h3>Education/Achievements</h3>
+                    <ul>
+                        <li>Good Shepherd Christian School - JHS</li>
+                        <li>Good Shephered Christian School - SHS</li>
+                        <li>Asia Pacific College - College (current)</li>
+                        <li>Graduated SHS with Honors</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>IT Experience</h3>
+                    <ul>
+                        <li>Basics of Python, Java, and HTML</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Hobbies and Interests</h3>
+                    <ul>
+                        <li>Basketball</li>
+                        <li>Volleyball</li>
+                        <li>Billiards</li>
+                        <li>Playing online games</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Goals in Life/Dream</h3>
+                    <ul>
+                        <li>My goal in life is to find a career I’m passionate about and work hard to be successful in it while building meaningful relationships and making a positive impact on the people around me.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Picture Gallery</h3>
+                    <table>
+                        <tr>
+                            <th><img src="https://media.discordapp.net/attachments/1303410468060266567/1319556517820760064/IMG_1385.jpg?ex=676cfbc6&is=676baa46&hm=49b9145b9573fbabcddb4d34e9c12f6137496ac03d9681c4cfa3d07c4905ee4c&=&format=webp&width=672&height=897"></th>
+                            <th><img src="https://media.discordapp.net/attachments/1303410468060266567/1319556518525272126/IMG_5999.jpg?ex=676cfbc6&is=676baa46&hm=fe5e7a4ae9dbe598a9d31f5f538b132e6f0269d208cfe5a3ff24f1a51f5a1328&=&format=webp&width=672&height=897"></th>
+                            <th><img src="https://media.discordapp.net/attachments/1303410468060266567/1319557990256349227/IMG_9278.jpg?ex=676cfd25&is=676baba5&hm=a7ab51189c1e16afe336560947684b37d3356111907e355fdc527d0040284bdd&=&format=webp&width=1593&height=895"></th>
+                            <th><img src="https://media.discordapp.net/attachments/1303410468060266567/1319558442209382420/IMG_3508.jpg?ex=676cfd90&is=676bac10&hm=69ef93a5685708a9f25e363875e817abab88c2b0b744ca16a68b8268e9e7657d&=&format=webp&width=672&height=897"></th>
+                        </tr>
+                    </table>
+                </div>
+            </section>
+            <aside class="sidebar">
+               
+            </aside>
+        </main>
+        <footer>
+            Asia Pacific College
+        </footer>
+    </div>
+</template>
+
+<script></script>
+<style></style>
